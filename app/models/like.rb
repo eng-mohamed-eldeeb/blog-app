7c0019a7d143f9ffceabbd9aac99023a0f48f4b7
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-20 20:40:08 +0200

    Fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    after_save :update_likes_counter
-    
-    private
+  after_save :update_likes_counter
 
-    def update_likes_counter
-        post.increment!(:likes_counter)
-    end
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
