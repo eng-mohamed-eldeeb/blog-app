dc42925e0ef9b028287fb89ab97b6ad147430644
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-20 17:45:34 +0200

    Create Likes and comments tables

---
+++ db/migrate/20230120153808_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.references :author, null: false, foreign_key: { to_table: :user }
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
