5c86d67077fbc9911e4638a5a6afb9854cf8f440
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-28 16:24:28 +0200

    Create the comment path and controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   end
   post '/users/:user_id/posts', to: 'posts#create', as: 'create_user_post'
   post '/users/:user_id/posts/:id/likes', to: 'likes#create', as: 'create_post_like'
-  
+  get '/posts/:post_id/comments/new', to: 'comments#new', as: 'new_post_comment'
+  post '/posts/:post_id/comments', to: 'comments#create', as: 'create_post_comment'
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
