6e48f9e32d2683291308d5aecf23bdfb1f2c373e
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-28 15:23:45 +0200

    Create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,33 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    @user = User.find_by(id: params[:user_id])
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    @comments = Comment.new
+  end
+  
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find_by_id(params[:id])
+    @comments = @post.comments
+  end
+  
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
   end
 
-  def show
-    @post = Post.find(params[:id])
-    @comments = @post.most_recent_comments
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to root_path notice: 'Post created successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
   end
-  root 'users#index'
+  post '/users/:user_id/posts', to: 'posts#create', as: 'create_user_post'
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index'
 end
