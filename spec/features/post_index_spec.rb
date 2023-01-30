78e1834ebaa83dc83fdd78ee295347426dc78e28
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-31 01:36:24 +0200

    Fix linter errors

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Post' , type: :feature do
+RSpec.describe 'Post', type: :feature do
   before(:each) do
     @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    @second_user = User.create(name: 'Peter', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @second_user = User.create(name: 'Peter', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Mexico.')
     @first_post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
     @second_post = Post.create(author: @user, title: 'Hi', text: 'This is my second post')
     @third_post = Post.create(author: @user, title: 'Hi', text: 'This is my third post')
